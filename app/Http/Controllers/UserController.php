c2fb82e361f5bc77b32d6c679b82c319cb056705
Fanani <azfann.natinons@gmail.com>
2026-03-03 20:04:04 +0700

    Test: Verifikasi hasil Praktikum 6 di browser dan berhasil melakukan update data user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,13 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // Coba akses model UserModel
-        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        // Langkah 8: Tambah data user dengan Eloquent Model
+        $data = [
+            'username' => 'customer-1',
+            'nama' => 'Pelanggan',
+            'password' => Hash::make('12345'), // Mengenkripsi password
+            'level_id' => 1
+        ];
+        UserModel::insert($data); // Tambahkan data ke tabel m_user
+        
+
+        // Langkah 10: Update data user
+        $dataUpdate = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($dataUpdate); // Update data user
+
+        // Ambil semua data dari tabel m_user menggunakan Eloquent
+        $user = UserModel::all(); 
         return view('user', ['data' => $user]);
     }
 }
